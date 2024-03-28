b908b527881d484a0a6fbf73cdda88ac713c5207
Sharon <Sharonaquilla28@gmail.com>
2024-03-28 15:29:13 +0300

    scope added

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(bob){
+    customerName = customerName.toUpperCase
+}
+
+function setBestCustomer(){
+    bestcustomer = 'not bob';
+}
+function overwriteBestCustomer (){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavouriteCustomer ='some other name'
+function changeLeastFavouriteCustomer(){
+  leaStFavouriteCustomer ='anothername'
+}
+
+
